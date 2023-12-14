4a19c24199cfdd07c9b0e2943820733aa73b4250
asktim <mail@asktim.ru>
2023-12-14 03:21:14 +0100

    Ensure we are storing json object

--- lib/services/statistics_recorder.rb
+++ lib/services/statistics_recorder.rb
@@ -4,9 +4,15 @@ module Services
   class StatisticsRecorder
     class << self
       def run(link:, request:)
-        link.visits.create!(agent: request.user_agent.to_json)
+        link.visits.create!(agent: request_data(request))
 
         link
+      end
+
+      private
+
+      def request_data(request)
+        { request_id: request.uuid, user_agent: request.user_agent, remote_ip: request.remote_ip }
       end
     end
   end
--- spec/services/statistics_recorder_spec.rb
+++ spec/services/statistics_recorder_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 RSpec.describe Services::StatisticsRecorder do
   let(:url) { Faker::Internet.url }
   let(:link) { Link.create(url:, signature: "sign") }
-  let(:request) { double(user_agent: { agent: "Firefox" }) }
+
+  let(:request) do
+    double(user_agent: "Firefox", uuid: "462b1399-3369-4460-b6a2-efa35e5f4d07", remote_ip: "1.1.1.1")
+  end
 
   subject { described_class.run(link:, request:) }
 
   it "increments visits count" do
     expect { subject }.to change { link.statistics[:visits_count] }.from(0).to(1)
+    expect(link.visits.last.agent).to include(
+      "user_agent" => "Firefox",
+      "remote_ip" => "1.1.1.1",
+      "request_id" => "462b1399-3369-4460-b6a2-efa35e5f4d07"
+    )
     expect { described_class.run(link:, request:) }.to change { link.statistics[:visits_count] }.from(1).to(2)
   end
 end
